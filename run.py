cb5cf6f1bb8f2fdf08d44f73f56253c0e8d06f33
Vivin Wilson <vivinwilson@hungerkind.local>
2018-10-26 15:45:29 -0400

    bugfix,cleanup

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ import datetime
 import urllib.request
 import base64
 import json
-import time
 
 os.system("python install_packages.py")
 
@@ -12,8 +11,10 @@ import pexpect
 
 ###################################################################################
 
-ASSIGNMENT = "assignment4"
+ASSIGNMENT = "Assignment4_Shell"
 DEADLINE = "2018-08-29 17:00:00"
+# compiles inside the repo itself 
+COMPILE_CMD = 'gcc -std=c99 -o "'+ASSIGNMENT+'/'+'shell" "'+ASSIGNMENT+'/shell.c'
 
 ORGS = 'Fall18Systems'
 API_TOKEN = '<generated token>'
@@ -21,13 +22,16 @@ GIT_API_URL = 'https://api.github.com'
 MYUSERNAME = ""
 MYPASSWORD = ""
 
+
 HOME = "/vagrant"
 PROMPT = ".*>>>$"
 ###################################################################################
 
 
 
-def get_repos(url):
+def _get_repos(url):
+    global GIT_API_URL,MYUSERNAME,API_TOKEN
+
     result = None
     request = urllib.request.Request(GIT_API_URL + url + '?per_page=200')
     base64string = base64.encodestring(('%s/token:%s' % (MYUSERNAME, API_TOKEN)).encode()).decode().replace('\n', '')
@@ -39,26 +43,11 @@ def get_repos(url):
     result.close()
     return res
 
-def get_tests(child):
-    global PROMPT,HOME
 
-    child.sendline("ls "+HOME+"/tests")
-    child.expect(PROMPT)
-    return " ".join(child.after.split("\r\n")[1:-1]).split()
+def _pull_git_repos(child):
+    global PROMPT,ORGS,HOME,MYUSERNAME,MYPASSWORD
 
-
-def pull_git_repos(child):
-    global PROMPT,CLONE_SCRIPT
-    child.sendline("")
-
-
-def get_git_repos(child):
-    global PROMPT,HOME
-
-    child.sendline(HOME+"/repos")
-    child.expect(PROMPT)
-
-    for r in get_repos:
+    for r in _get_repos('/orgs/' + ORGS + '/repos'):
         child.sendline('git clone https://' + MYUSERNAME + ':' + MYPASSWORD + '@github.com/' + ORGS + '/' + r['name']+" &\n\n")
         child.expect(PROMPT)
 
@@ -68,12 +57,24 @@ def get_git_repos(child):
         child.expect(PROMPT)
         if not " ".join(child.after.split("\r\n")[1:-1]).split():
             break
-        time.sleep(5)
+
+
+def get_repos(child):
+    global PROMPT,HOME
+
+    child.sendline("cd "+HOME+"/repos")
+    child.expect(PROMPT)
+
+    _pull_git_repos(child)
 
     child.sendline("ls ")
     child.expect(PROMPT)
+    ret = " ".join(child.after.split("\r\n")[1:-1]).split()
 
-    return " ".join(child.after.split("\r\n")[1:-1]).split()
+    child.sendline("cd "+HOME)
+    child.expect(PROMPT)
+
+    return ret
 
 
 def process_late_submission(child):
@@ -85,17 +86,49 @@ def process_late_submission(child):
     date = child.after.split("\r\n")[1].strip()
     sd = datetime.datetime.strptime(date.split(": ")[1].strip(),'%Y-%m-%d %H:%M:%S')
     dd = datetime.datetime.strptime(DEADLINE,'%Y-%m-%d %H:%M:%S')
+
     if sd > dd:
         late_submission = "submission delayed by : "+str(sd - dd)
 
     return late_submission
  
+
+def execute(child,test,output_file):
+    child.sendline("python "+test+" >> "+output_file)
+    child.expect(PROMPT)  
+
+
+def compile_repo(child):
+   child.sendline("ls | grep Makefile ")
+   child.expect(PROMPT)
+
+   if not " ".join(child.after.split("\r\n")[1:-1]).split():
+       child.sendline(COMPILE_CMD)
+       child.expect(PROMPT)
+       return 
     
+   child.sendline("make")
+   child.expect(PROMPT)
+
+
+def copy_tests(child,tests):
+    for test in tests:
+        child.sendline("cp "+HOME+"/tests/"test+" .")
+        child.expect(PROMPT)
+
+
+def get_tests(child):
+    global PROMPT,HOME
+
+    child.sendline("ls "+HOME+"/tests")
+    child.expect(PROMPT)
+    return " ".join(child.after.split("\r\n")[1:-1]).split()
+
+
 if __name__ == "__main__":
     child = pexpect.spawn("bash")
     child.sendline("export PS1='>>>'")
     child.expect(PROMPT)
-
     # cleanup workdir 
     child.sendline("rm -rf output")
     child.expect(PROMPT)
@@ -106,11 +139,16 @@ if __name__ == "__main__":
     
     tests = get_tests()
 
-    for repo in get_git_repos(child):
-        print "Testing submission :",repo
 
-        child.sendline("cd "+HOME+"/repos/"+repo+"/"+ASSIGNMENT)
+    for repo in get_repos(child):
+        print "Testing submission :",repo
+        _repo = HOME+"/repos/"+repo+"/"+ASSIGNMENT
+        child.sendline("cd "+_repo)
         child.expect(PROMPT)
+        
+        compile_repo(child)
+        copy_tests(child,tests)
+
         output_file = HOME+"/output/"+repo+"_output.txt"
         child.sendline("touch "+output_file)
         ls = process_late_submission(child)
@@ -118,7 +156,6 @@ if __name__ == "__main__":
         child.expect(PROMPT)
 
         for test in tests:
-
             print " "*3,"Running",test
             child.sendline("echo '"+"="*50+"' >> "+output_file)
             child.expect(PROMPT)
@@ -126,8 +163,7 @@ if __name__ == "__main__":
             child.expect(PROMPT)
             child.sendline("echo '"+"="*50+"' >> "+output_file)
             child.expect(PROMPT)
-            child.sendline("python "+test+" >> "+output_file)
-            child.expect(PROMPT)
+            execute(child,test,output_file)
             child.sendline("echo '"+"="*50+"' >> "+output_file)
             child.expect(PROMPT)
             child.sendline("echo '' >> "+output_file)
